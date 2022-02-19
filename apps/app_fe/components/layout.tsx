a285f9bfbf5c81e7fab0d7e77ea400bd1c0b6496
milanbella <milanbella@gmail.com>
2022-02-19 16:20:35 +0000

    m

--- apps/app_fe/components/layout.tsx
+++ apps/app_fe/components/layout.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import styles from "../styles/components/Layout.module.scss";
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 
 const Layout: NextPage = ({ children }) => {
   return (
